f478e2ebddaad76db3e7860ee387d695414d9ab1
Sergey Pchelintsev <linz.sergey@gmail.com>
2022-12-26 18:10:31 +0200

    Init commit

---
+++ lib/builder_io/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module BuilderIo
+  VERSION = "0.1.0"
+end
---
+++ spec/builder_io_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe BuilderIo do
+  it "has a version number" do
+    expect(BuilderIo::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
